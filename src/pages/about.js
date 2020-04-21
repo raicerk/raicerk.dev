a108928f1430fb03a8f354c50c1c2e54e5cf4eb6
Juan Mora <raicerk@gmail.com>
2020-04-21 02:02:27 -0300

    Se agrega sitio de about

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <div style={{ color: `teal` }}>
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+  </div>
+    <Link to="/">Go to home</Link>
+  </Layout>
+)
+
+export default AboutPage
