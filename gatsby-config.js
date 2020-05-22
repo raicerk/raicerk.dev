43b00b006ff1c9df1e151468a776b2fb69c622fa
Juan Mora <raicerk@gmail.com>
2020-05-22 02:23:23 -0400

    Update de metadatos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `raicerk.dev`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Un blog de programación y tecnología`,
     author: `@raicerk`,
     subtitle: `Un blog de un ñoño para ñoños`
   },
